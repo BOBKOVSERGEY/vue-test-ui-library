ff304a7ac86c10b1d9b2c0307b212f74bb3be824
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2024-04-26 14:52:38 +0300

    ready prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import './assets/main.scss'
 
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
+import Menubar from 'primevue/menubar';
+// @ts-ignore
+import Wind from "@/presets/wind/index.js";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +13,11 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: Wind
+});
+
+app.component('MenuBar', Menubar)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,106 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
+import Panel from 'primevue/panel';
+import Skeleton from 'primevue/skeleton';
+
+const items = ref([
+  {
+    label: 'Home',
+    icon: 'pi pi-home'
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star'
+  },
+  {
+    label: 'Projects',
+    icon: 'pi pi-search',
+    items: [
+      {
+        label: 'Components',
+        icon: 'pi pi-bolt'
+      },
+      {
+        label: 'Blocks',
+        icon: 'pi pi-server'
+      },
+      {
+        label: 'UI Kit',
+        icon: 'pi pi-pencil'
+      },
+      {
+        label: 'Templates',
+        icon: 'pi pi-palette',
+        items: [
+          {
+            label: 'Apollo',
+            icon: 'pi pi-palette'
+          },
+          {
+            label: 'Ultima',
+            icon: 'pi pi-palette'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope'
+  }
+]);
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="p-4">
+    <MenuBar :model="items"></MenuBar>
+    <Card>
+      <template #title> Simple Card </template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+          quas!
+        </p>
+      </template>
+    </Card>
+    <div class="card">
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+
+      <Divider />
+
+      <p class="m-0">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+        voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+      </p>
+
+      <Divider />
+
+      <p class="m-0">
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia
+        deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+      </p>
+
+      <Divider />
+
+      <p class="m-0">
+        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+        maiores alias consequatur aut perferendis doloribus asperiores repellat. Donec vel volutpat ipsum. Integer nunc magna, posuere ut tincidunt eget, egestas vitae sapien. Morbi dapibus luctus odio.
+      </p>
+    </div>
+    <Panel header="Lorem">
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </Panel>
+    <Skeleton class="mb-2"></Skeleton>
+    <Skeleton width="10rem" class="mb-2"></Skeleton>
+    <Skeleton width="5rem" class="mb-2"></Skeleton>
+    <Skeleton height="2rem" class="mb-2"></Skeleton>
+    <Skeleton width="10rem" height="4rem"></Skeleton>
   </div>
 </template>
